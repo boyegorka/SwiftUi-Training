b4e4c5a392ab30dae8937a42811d0ebdbe38ce3d
boyegorka <boyegorka@gmail.com>
2022-06-09 22:19:17 +0300

    Deleted extra files

--- MyPlayground.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-var adress = ["Nick":"NewYork","Mark":"Germany", "Ann":"Russia"]
-print(adress["Nick"])
-adress.updateValue("German", forKey: "Mark2")
-print(adress.count)
-print(adress)
-
-adress.removeValue(forKey: "German")
-print(adress)
